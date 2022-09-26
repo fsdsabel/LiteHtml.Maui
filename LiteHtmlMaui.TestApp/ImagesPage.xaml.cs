966f306b7e05d3e49f77326c665d95d161680cc9
Daniel Sabel <msdn.sabel@fsd-web.de>
2022-09-26 08:21:01 +0200

    fixed sample

--- LiteHtmlMaui.TestApp/ImagesPage.xaml.cs
+++ LiteHtmlMaui.TestApp/ImagesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using LiteHtmlMaui.Controls;
+using System.Windows.Input;
 
 namespace LiteHtmlMaui.TestApp;
 
@@ -8,6 +9,25 @@ public partial class ImagesPage : ContentPage
 	public ImagesPage()
 	{
 		InitializeComponent();
+    }
+
+    private async void HtmlComparisonView_HtmlLinkClicked(object sender, HtmlLinkClickedEventArgs e)
+    {
+        switch (e.Url)
+        {
+            case "#top":
+                await FindScrollView(sender as LiteHtml)?.ScrollToAsync(0, 0, true);
+                break;
+            default:
+                await App.Current.MainPage.DisplayAlert("Link clicked", e.Url, "OK");
+                break;
+        }
+    }
+
+    private ScrollView? FindScrollView(Element? view)
+    {
+        if (view is ScrollView || view == null) return view as ScrollView;
+        return FindScrollView(view.Parent);
     }
 
 }
