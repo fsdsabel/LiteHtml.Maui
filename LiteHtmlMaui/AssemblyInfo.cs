14a467ba5d9e0b002c1404435e95b7fd3cd235e0
Daniel Sabel <daniel.sabel@fsd-web.de>
2024-12-20 13:48:32 +0100

    update ios and android binaries, prepare 1.1.0

--- LiteHtmlMaui/AssemblyInfo.cs
+++ LiteHtmlMaui/AssemblyInfo.cs
@@ -1,30 +1,3 @@
 ﻿using Microsoft.Maui.Controls;
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/2021/mauilitehtml", "LiteHtmlMaui.Controls")]
-
-#if IOS
-[assembly: ObjCRuntime.LinkWith(
-        "libgumbo.a",
-        ForceLoad = true,
-        LinkerFlags = "",
-        IsCxx = true,
-        Frameworks = ""
-    )
-]
-[assembly: ObjCRuntime.LinkWith(
-        "liblitehtml.a",
-        ForceLoad = true,
-        LinkerFlags = "",
-        IsCxx = true,        
-        Frameworks = ""
-    )
-]
-[assembly: ObjCRuntime.LinkWith(
-        "liblitehtml-maui.a",
-        ForceLoad = true,
-        LinkerFlags = "",
-        IsCxx = true,
-        Frameworks = ""
-    )
-]
-#endif
